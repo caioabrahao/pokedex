7dda7f7c8cc2c989f1b037dfcf14d0179763683f
Caio Abrahão <contato.caioabrahao@gmail.com>
2024-12-22 23:38:44 -0300

    Add Pokédex layout and PokemonCard component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,33 @@
 <script setup>
+import PokemonCard from './components/PokemonCard.vue';
 
 </script>
 
 <template>
-  
+    <main>
+        <section class="section">
+            <h1>Pokédex</h1>
+            <div class="pokemonList">
+                <PokemonCard></PokemonCard>
+                <PokemonCard></PokemonCard>
+                <PokemonCard></PokemonCard>
+                <PokemonCard></PokemonCard>
+                <PokemonCard></PokemonCard>
+            </div>
+        </section>
+    </main>
 </template>
 
 <style scoped>
+    .section{
+        padding: 32px;
+        text-align: center;
+    }
 
+    .pokemonList{
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+        gap: 1rem;
+        padding: 32px;
+    }
 </style>
---
+++ src/components/PokemonCard.vue
@@ -0,0 +1,36 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="pokemonCard">
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur">
+        <h2>Bulbasaur</h2>
+        <p>Grass</p>
+    </div>
+</template>
+
+<style scoped>
+    .pokemonCard{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+
+        border: 1px solid #000000;
+        border-radius: 5px;
+        padding: 1rem;
+        text-align: center;
+        width: 200px;
+
+        transition: all 0.3s;
+    }
+    .pokemonCard:hover{
+        background-color: #f0f0f0;
+    }
+
+    .pokemonCard img{
+        width: 100px;
+        height: 100px;
+    }
+</style>
